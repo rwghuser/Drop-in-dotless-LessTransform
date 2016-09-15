f9676e4b8d6ad74a39d76aedfd29de320ffab465
github_user83 <richw.sf@gmail.com>
2016-09-15 14:33:38 -0400

    typo fix
    updated instructions for uniformity

--- LessTransform.cs
+++ LessTransform.cs
@@ -8,7 +8,7 @@ namespace YourNamespaceHere.App_Start
 {
     public class LessTransform : IBundleTransform
     {
-        //todo for LessTransform: cutpaste to BundleConfig:  StackflowQuizzio.App_Start.LessTransform.AdddotLessTransformToBundleCollection(bundles, true);
+        //todo for LessTransform: cutpaste to BundleConfig:  YourNamespaceHere.App_Start.LessTransform.AdddotLessTransformToBundleCollection(bundles, true);
         //todo for LessTransform: copypaste where necessary, such as _layout.cshtml:  @Styles.Render("~/Content/lesscss")
 
         public static void AdddotLessTransformToBundleCollection(BundleCollection argBC, bool argMinify)
